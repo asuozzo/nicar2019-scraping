d7dacccd122063a4bfe4586deb1e5b096cc02f58
asuozzo <asuozzo@gmail.com>
2019-01-25 18:26:06 -0500

    skeleton candidate list/profile link setup

--- scraping_site/candidate_profiles.py
+++ scraping_site/candidate_profiles.py
@@ -5,3 +5,15 @@ bp = Blueprint('candidate_profiles', __name__)
 @bp.route('/3')
 def candidate_profiles():
     return render_template('candidate-profiles.html')
+
+
+@bp.route("/3/<candidate_name>")
+def candidate_profile(candidate_name):
+    # TODO: get candidate info from data_api
+    try:
+        return render_template('candidate-profile.html', 
+                    candidate_name=candidate_name,
+                    candidate_info=candidate_info)
+    except NameError:
+        return render_template('candidate-missing.html', candidate_name=candidate_name)
+        
