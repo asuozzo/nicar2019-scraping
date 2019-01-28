a21f1fa6ea6600d9c4408ff32a3b30175711f643
asuozzo <asuozzo@gmail.com>
2019-01-28 09:36:28 -0500

    link candidate table to candidate bio page with more information

--- scraping_site/candidate_profiles.py
+++ scraping_site/candidate_profiles.py
@@ -1,19 +1,23 @@
 from flask import Blueprint, render_template
+from . import data_api
 
 bp = Blueprint('candidate_profiles', __name__)
 
 @bp.route('/3')
 def candidate_profiles():
-    return render_template('candidate-profiles.html')
+    results = data_api.get_race_results(2018, "US SENATOR")
+    return render_template(
+        'candidate-profiles.html',
+        results=results
+        )
 
 
 @bp.route("/3/<candidate_name>")
 def candidate_profile(candidate_name):
-    # TODO: get candidate info from data_api
-    try:
+    candidate_info = data_api.get_candidate_info(candidate_name)
+    if candidate_info:
         return render_template('candidate-profile.html', 
-                    candidate_name=candidate_name,
                     candidate_info=candidate_info)
-    except NameError:
+    else:
         return render_template('candidate-missing.html', candidate_name=candidate_name)
         
