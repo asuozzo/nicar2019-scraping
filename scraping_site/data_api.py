4266bf1baa0a7a6cc4c069729169811f42b28539
Geoff Hing <ghing@apmreports.org>
2019-02-01 22:49:41 -0600

    Combine results data into one CSV
    Addresses #14
    https://github.com/asuozzo/nicar2019-scraping/issues/14

--- scraping_site/data_api.py
+++ scraping_site/data_api.py
@@ -2,13 +2,10 @@ import csv
 import os
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
-
-def path_for_year(year):
-    filename = "electionsummary{}.csv".format(year)
-    return os.path.join(DATA_DIR, filename)
+RESULTS_PATH = os.path.join(DATA_DIR, 'results.csv')
 
 def get_race_results(year, office):
-    with open(path_for_year(year)) as f:
+    with open(RESULTS_PATH) as f:
         return [
             result for result in csv.DictReader(f)
             if result['office'] == office
