2e9a943c71b80c4ddbd1f3c593c678ecad83dec1
Geoff Hing <ghing@apmreports.org>
2019-02-02 08:40:15 -0600

    Paginated results accessed via a form
    Addresses #14
    https://github.com/asuozzo/nicar2019-scraping/issues/14

--- scraping_site/data_api.py
+++ scraping_site/data_api.py
@@ -1,4 +1,5 @@
 import csv
+import math
 import os
 
 DATA_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'data')
@@ -8,11 +9,38 @@ def get_race_results(year, office):
     with open(RESULTS_PATH) as f:
         return [
             result for result in csv.DictReader(f)
-            if result['office'] == office
+            if (result['year'] == year and
+                result['office'] == office)
         ]
+
 
 def get_candidate_info(candidate_name):
     filename = os.path.join(DATA_DIR, 'candidateprofiles.csv')
     with open(filename) as f:
         return next((result for result in csv.DictReader(f)
             if result['candidate'] == candidate_name), None)
+
+
+def get_years_offices():
+    years = set()
+    offices = set()
+
+    with open(RESULTS_PATH) as f:
+        for result in csv.DictReader(f):
+            years.add(result['year'])
+            offices.add(result['office'])
+
+        return sorted(list(years)), sorted(list(offices))
+
+
+def paginate(results, page, page_size=10):
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    next_page = page + 1
+    total_pages = int(math.ceil(len(results) / (page_size * 1.0)))
+
+    if end_index > len(results):
+        end_index = None
+        next_page = None
+
+    return results[start_index:end_index], next_page, total_pages
--- scraping_site/form_with_url_params.py
+++ scraping_site/form_with_url_params.py
@@ -1,9 +1,42 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for
+
+from .data_api import get_race_results, get_years_offices, paginate
+
 
 bp = Blueprint('form_with_url_params', __name__)
 
+
+@bp.app_template_global('url_for_other_page')
+def url_for_other_page(page):
+    args = request.args.copy()
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+
+
 @bp.route('/4')
 def form_with_url_params():
-     """Form that selects race from dropdown. Submitting form shows table with race as URL param"""
-     # Also think about pagination here
-     return "TODO: Implement this"
+    """Form that selects race from dropdown. Submitting form shows table with race as URL param"""
+    year = request.args.get('year')
+    office = request.args.get('office')
+
+    if year is not None and office is not None:
+        results = get_race_results(year, office)
+        page = int(request.args.get('page', 1))
+        paginated, next_page, total_pages = paginate(results, page)
+
+        return render_template(
+            'results-paged.html',
+            results=paginated,
+            year=year,
+            office=office,
+            page=page,
+            next_page=next_page,
+            total_pages=total_pages
+        )
+
+    years, offices = get_years_offices()
+    return render_template(
+        'results-form.html',
+        years=years,
+        offices=offices
+    )
