ca18feeabcea939423edd0c0a3a1fcd68e4c48e5
Geoff Hing <ghing@apmreports.org>
2019-03-01 17:17:13 -0600

    Clean up style and update return values
    Clean up trailing whitespace, quoting and spacing around arguments and
    dictionary keys and values.
    
    Update the function that scrapes the results to return an empty list on
    a 404 to avoid having to do the value check in the calling code.
    
    Adddresses #21
    https://github.com/asuozzo/nicar2019-scraping/issues/21

--- scrapers-solutions/parse_url_param_form.py
+++ scrapers-solutions/parse_url_param_form.py
@@ -8,27 +8,33 @@ import sys
 import requests
 from bs4 import BeautifulSoup
 
+
 def get_params_lists(content):
     soup = BeautifulSoup(content, 'html.parser')
 
-    office_select = soup.find("select", {"id":"office"})
-    offices = [office.text for office in office_select.find_all("option")]
-    
-    year_select = soup.find("select", {"id": "year"})
-    years = [year.text for year in year_select.find_all("option")]
+    office_select = soup.find('select', {'id': 'office'})
+    offices = [office.text for office in office_select.find_all('option')]
+
+    year_select = soup.find('select', {'id': 'year'})
+    years = [year.text for year in year_select.find_all('option')]
 
     return offices, years
 
+
 def get_results(url, year, office):
-    payload = {'year':year, "office":office}
+    payload = {
+        'year': year,
+        'office': office,
+    }
     r = requests.get(url, params=payload)
 
     if r.status_code == 404:
-        return None
+        return []
+
     else:
         soup = BeautifulSoup(r.text, 'html.parser')
-        table = soup.find("table")
-        
+        table = soup.find('table')
+
         columns = []
         header_cols = table.find('thead').find_all('th')
         for header_col in header_cols:
@@ -42,32 +48,33 @@ def get_results(url, year, office):
                 row[columns[i]] = td.string
                 i += 1
             if len(row) > 0:
-                row["Year"] = year
-                row["Office"] = office
+                row['Year'] = year
+                row['Office'] = office
                 results.append(row)
 
         return results
 
-if __name__ == "__main__":
-    
+
+if __name__ == '__main__':
     url = sys.argv[1]
     r = requests.get(url)
-    
+
     offices, years = get_params_lists(r.content)
 
     results = []
     for year in years:
         for office in offices:
             race_results = get_results(url, year, office)
-            if race_results:
-                results += race_results
+            results += race_results
 
-    columns = ["Year","Office","Candidate","Party","Votes"]
+    columns = [
+        'Year',
+        'Office',
+        'Candidate',
+        'Party',
+        'Votes'
+    ]
     writer = csv.DictWriter(sys.stdout, fieldnames=columns)
     writer.writeheader()
     for result in results:
         writer.writerow(result)
-
-
-
-
