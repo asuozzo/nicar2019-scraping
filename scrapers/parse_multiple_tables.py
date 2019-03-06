48c0256894a61580686f9d78ccb0ec6d899c2ef1
asuozzo <asuozzo@gmail.com>
2019-03-05 21:21:20 -0500

    multiple-table skeleton file implementation

---
+++ scrapers/parse_multiple_tables.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+"""
+Parse a page with multiple HTML tables and output CSV to standard output
+
+Example:
+
+    python fetch_html.py http://localhost:5000/2 | \
+    python parse_multiple_tables.py > data/results.csv
+
+"""
+
+import csv
+import sys
+
+from bs4 import BeautifulSoup
+
+
+if __name__ == "__main__":
+
+    # Just like last time around, we'll get the html we fetched
+    # and create a parser object
+    soup = BeautifulSoup(sys.stdin, 'html.parser')
+
+    # If we're going to scrape all these tables into one file, 
+    # we'll need to keep track of which table each row came from. 
+    # Let's put in a first column called "Office".
+    columns = ["Office"]
+
+    # This is the same thing we did in the single table file — 
+    # they all have the same header row, so we only need to grab
+    # the column names
+    header_cols = soup.find("table").find('thead').find_all('th')
+    for header_col in header_cols:
+        columns.append(header_col.string)
+
+    writer = csv.writer(sys.stdout)
+    writer.writerow(columns)
+
+    # FILL IN THE BLANK: Find each header on this table, and create 
+    # a for loop that iterates through them and grabs the name of 
+    # the office.
+
+    
+    # FILL IN THE BLANK: Now you can find each table and parse it
+    # like you did with just one table. Within the same for loop, 
+    # find the table immediately after each header.
+    # 
+    # (Hint: See what find_next() does)
+
+
+    # Next, add the office variable to the beginning of the row
+    # data you just collected and write that row to your file.
+            row.insert(0, office)
+            writer.writerow(row)
+
