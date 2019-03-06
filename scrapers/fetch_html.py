951faff4704759a76c3e8c065f87ad7a884b90c5
asuozzo <asuozzo@gmail.com>
2019-03-05 20:34:36 -0500

    single-table scraper skeleton file implementation

---
+++ scrapers/fetch_html.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+"""
+Fetch HTML from a URL and output to standard output.
+
+Example:
+    python fetch_html.py http://localhost:5000/1
+
+"""
+
+import sys
+
+import requests
+
+def fetch_url(url):
+    # FILL IN THE BLANK: Make a request to this url 
+    # and return the text.
+    r = requests.get(url)
+    return r.text
+
+
+if __name__ == '__main__':
+
+    # FILL IN THE BLANK: Get the url you passed in on 
+    # the command line.
+
+    # FILL IN THE BLANK: Use our fetch_url function to 
+    # get this page's contents
+
+    # FILL IN THE BLANK: Write the results to standard 
+    # output so we can save it to a file or send it to 
+    # another function.
---
+++ scrapers/parse_single_table.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+"""
+Parse a single HTML table and output CSV to standard output
+
+Example:
+    python fetch_html.py http://localhost:5000/1 | \
+    python parse_single_table.py > data/results.csv
+
+"""
+
+import csv
+import sys
+
+from bs4 import BeautifulSoup
+
+
+if __name__ == "__main__":
+
+    # FILL IN THE BLANK: Create a Beautiful Soup object 
+    # from standard input (sys.stdin)
+
+
+    # FILL IN THE BLANK: There's only one table on this 
+    # page. Find it.
+
+
+    columns = []
+    # FILL IN THE BLANK: Next, we want to fill this list 
+    # with column names from the table. Check it out in the 
+    # page source. Is there a way to select each column's name?
+
+
+    # Here, we'll create a csv writer that writes to standard
+    # output. This writes output to the terminal unless we 
+    # redirect the output to a filename. 
+    # That way, we can pass in lists (like our list of columns) 
+    # with writer.writerow(LIST) to add new rows to the file.
+    writer = csv.writer(sys.stdout)
+    writer.writerow(columns)
+
+
+    # FILL IN THE BLANK: Write a "for" loop that goes through 
+    # each table row and writes each cell value to a list. 
+    # Then use "writer.writerow()" to write that list to a file.
+
