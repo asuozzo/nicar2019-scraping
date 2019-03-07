d50ee69be3c064a1622399c2752ff2ab03a6290d
asuozzo <asuozzo@gmail.com>
2019-03-06 17:52:21 -0800

    whoops! take the solution out of the file.

--- scrapers/fetch_html.py
+++ scrapers/fetch_html.py
@@ -15,8 +15,7 @@ import requests
 def fetch_url(url):
     # FILL IN THE BLANK: Make a request to this url 
     # and return the text.
-    r = requests.get(url)
-    return r.text
+    
 
 
 if __name__ == '__main__':
@@ -24,8 +23,10 @@ if __name__ == '__main__':
     # FILL IN THE BLANK: Get the url you passed in on 
     # the command line.
 
+
     # FILL IN THE BLANK: Use our fetch_url function to 
     # get this page's contents
+
 
     # FILL IN THE BLANK: Write the results to standard 
     # output so we can save it to a file or send it to 
