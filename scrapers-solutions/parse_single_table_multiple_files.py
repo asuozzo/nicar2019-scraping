987f5ffa670b4df0a4822b3bccdab4f8666cb0ca
Geoff Hing <ghing@apmreports.org>
2019-03-07 11:01:13 -0800

    Discover filenames from year and office CSV
    Instead of passing each filename as a positional command line argument,
    use the scraped CSV of years and offices to discover the filenames. This
    also lets us include the year and office in the output CSV.
    
    Addresses #31
    https://github.com/asuozzo/nicar2019-scraping/issues/31

--- scrapers-solutions/parse_single_table_multiple_files.py
+++ scrapers-solutions/parse_single_table_multiple_files.py
@@ -1,24 +1,46 @@
 #!/usr/bin/env python
 
 """
-Parse a single HTML table and output CSV to standard output.
+Parse HTML files containing a single results table and output CSV to standard
+output.
 
 The parsing logic is very similar to that in `parse_single_table.py` since the
 results in both versions of the results site that use forms are
 displayed in a single table per page.
 
-However, this version can parse multiple files, specified as positional
-arguments to scrape results from multiple HTML files.
+However, this version can parse multiple files. They are discovered based on
+the election years and offices scraped into a CSV. While we could just pass
+the filenames as positional arguments, the year of the result is not contained
+in the HTML of the results.
 
 Example:
-    ./scrapers-solutions/parse_single_table_multiple_files.py data/src/*.htm
+    ./scrapers-solutions/parse_single_table_multiple_files.py
+    data/years_offices.csv data/src
 
 """
 
 import csv
+import glob
+import os
+import re
 import sys
 
 from bs4 import BeautifulSoup
+
+
+def get_output_filename_wildcard(year, office):
+    """
+    Returns a normalized wildcard that will match filenames for
+    downloaded results HTML.
+
+    This returns a wildcard to accomodate multiple pages of results.
+
+    """
+    office_slug = office.strip().lower()
+    office_slug = re.sub(r'[^\w\s]', '', office_slug)
+    office_slug = re.sub(r'\s+', '_', office_slug)
+
+    return '{0}__{1}*.html'.format(year, office_slug)
 
 
 def parse_html(f):
@@ -57,43 +79,60 @@ def parse_html(f):
 
 
 if __name__ == "__main__":
-    # This implementation is pretty simple, so it's all in the main
-    # body of this script. For a more complicated scraper, it's important
-    # to break the steps into multiple functions.
+    # The first positional argument is the path to a CSV of years and offices
+    # scraped from the form.
+    years_offices_csv_path = sys.argv[1]
+    # The second positional argument is the path to a directory that contains
+    # downloaded HTML files.
+    html_search_path = sys.argv[2]
 
-    if len(sys.argv) == 1:
-        # No arguments, read HTML from standard input.
-        files_or_paths = [sys.stdin]
+    with open(years_offices_csv_path) as f:
+        reader = csv.DictReader(f)
 
-    else:
-        # Command line arguments. Interpret as paths to HTML files
-        files_or_paths = sys.argv[1:]
+        # Create an object that we'll use to write CSV.
+        # We'll write to standard output. If we want to output this to a file, we
+        # can just use the shell's redirection.
+        writer = None
 
+        # The CSV just contains rows of years and office names.
+        # Loop through them to identify filenames of scraped HTML files.
+        for row in reader:
+            election_year = row['year']
+            office = row['office']
+            # Build a wildcard that will match any HTML files that begin
+            # with the slug for the election year and the office.
+            output_pattern = os.path.join(
+                html_search_path,
+                get_output_filename_wildcard(election_year, office)
+            )
 
-    # Create an object that we'll use to write CSV.
-    # We'll write to standard output. If we want to output this to a file, we
-    # can just use the shell's redirection.
-    writer = None
+            # glob.glob() returns an iterable of filenames that match a
+            # pattern. We need to use this because we might have paged
+            # results.
+            # Not every year and office combination will have results, so
+            # this iterable might be empty in some cases.
+            for path in glob.glob(output_pattern):
+                with open(path) as f:
+                    results = parse_html(f)
 
-    for i, file_or_path in enumerate(files_or_paths):
-        # Loop through the paths or file objects and scrape the results from
-        # each.
-        try:
-            # Try to interpret the value as a string containing a path to an
-            # HTML file.
-            with open(file_or_path) as f:
-                results = parse_html(f)
+                    if writer is None:
+                        # We're handling the first file.
+                        # Write the header of our output CSV.
+                        # The year and office aren't in the HTML. So, we have
+                        # to add them in.
+                        fieldnames = (
+                            ['Year', 'Office'] +
+                            list(results[0].keys())
+                        )
 
-        except TypeError:
-            # Trying to open the path failed. Assume it's already a
-            # file-like-object, like sys.stdin
-            results = parse_html(file_or_path)
+                        writer = csv.DictWriter(sys.stdout,
+                            fieldnames=fieldnames)
+                        writer.writeheader()
 
-        if i == 0:
-            # We're handling the first file. Write the header of our output CSV.
-            writer = csv.DictWriter(sys.stdout, fieldnames=results[0].keys())
-            writer.writeheader()
-
-        for result in results:
-            # For each row of results, write a row of CSV.
-            writer.writerow(result)
+                    for result in results:
+                        # For each row of results, write a row of CSV.
+                        # Again, the results HTML doesn't include the election
+                        # year or office, so we have to add it in.
+                        result['Year'] = election_year
+                        result['Office'] = office
+                        writer.writerow(result)
